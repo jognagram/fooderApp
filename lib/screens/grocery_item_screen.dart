666ba4541eaf506eb21a00b00d0a759f9f38c931
Jordan Erifried Gnamien <61625884+Jordan-erifried@users.noreply.github.com>
2021-12-24 09:10:54 +0000

    ChoiceChip

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fooder_app/models/models.dart';
 
-class GroceryItemsScreen extends StatefulWidget {
+class GroceryItemScreen extends StatefulWidget {
+
   final Function(GroceryItem) onCreate;
   final Function(GroceryItem) onUpdate;
   final GroceryItem? originalItem;
- final bool isUpdating;
+  final bool isUpdating;
 
-  const GroceryItemsScreen({
+  const GroceryItemScreen({
     Key? key,
     required this.onCreate,
     required this.onUpdate,
@@ -15,11 +16,42 @@ class GroceryItemsScreen extends StatefulWidget {
 
   }) : isUpdating = (originalItem != null), super(key: key);
 
+
   @override
-  _GroceryItemsScState createState() => _GroceryItemsScState();
+  _GroceryItemScreenState createState() => _GroceryItemScreenState();
 }
 
-class _GroceryItemsScState extends State<GroceryItemsScreen> {
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final originalItem = widget.originalItem;
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSliderValue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.orange,);
--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fooder_app/models/models.dart';
+import 'package:fooder_app/screens/grocery_item_screen.dart';
 import 'package:provider/provider.dart';
 import 'empty_grocery_screen.dart';
+
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -12,7 +14,20 @@ class GroceryScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-
+          final manager = Provider.of<GroceryManager>(
+            context,
+            listen: false, );
+          Navigator.push(context,
+           MaterialPageRoute(
+            builder: (context) => GroceryItemScreen(
+              onCreate: (item) {
+                manager.addItem(item);
+                Navigator.pop(context);
+              },
+              onUpdate: (item) {},
+            ),
+           )
+          );
         },
       ),
       body: buildGroceryScreen(),
